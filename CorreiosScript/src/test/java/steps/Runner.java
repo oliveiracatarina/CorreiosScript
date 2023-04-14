54fd309b0198d8ffa1305bff5a3a4750e3844fe3
Catarina Oliveira <catarina.oliveira003@gmail.com>
2023-04-14 00:50:31 -0300

    finalizando

---
+++ CorreiosScript/src/test/java/steps/Runner.java
@@ -0,0 +1,12 @@
+package steps;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/feature/CorreiosScript.feature")
+
+public class Runner {
+}
+
